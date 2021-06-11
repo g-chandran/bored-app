ddcb1184347e63e7930cc43114bbf2d30067701d
G Hemachandran <chandranchan321@gmail.com>
2021-06-11 22:28:02 +0530

    Initial Commit + Customized defaults

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  let name = "chandran";
+</script>
+
+<div>
+  <p>Bored</p>
+  <p>Hello {name}</p>
+  <h1>Options</h1>
+  <h2>diy</h2>
+  <h3>Educational</h3>
+</div>
+
+<style>
+  p {
+    color: var(--orange);
+    text-transform: capitalize;
+    font-size: var(--big-font);
+    font-weight: 500;
+  }
+
+  h1 {
+    color: var(--black);
+    font-size: var(--med-font);
+  }
+
+  h2 {
+    color: var(--orange);
+    font-size: var(--small-font);
+  }
+
+  h3 {
+    font-size: var(--tiny-font);
+    font-weight: normal;
+  }
+</style>
